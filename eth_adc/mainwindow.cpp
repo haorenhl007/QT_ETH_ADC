36e14706560cfc5244322ad3f77f7419865fa992
nowhard <nowhard@mail.ru>
2016-11-10 10:27:43 +0300

    Revert "Запись в файл"
    This reverts commit 0a54863b7a23abc38c029920f1ff78f75eda61d1.

--- eth_adc/mainwindow.cpp
+++ eth_adc/mainwindow.cpp
@@ -1,37 +1,59 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QFile>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    stateProc=false;
+    udpSocket = new QUdpSocket(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    if(stateProc==true)
+    {
+        datafile.flush();
+        datafile.close();
+    }
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    udpSocket = new QUdpSocket(this);
-    udpSocket->bind(QHostAddress::Any, 40);
-    connect(udpSocket, SIGNAL(readyRead()), SLOT(read()));
+
+    if(stateProc==false)
+    {
+        datafile.setFileName("data.bin");
+        datafile.open(QFile::Append);
+        udpSocket->bind(QHostAddress::Any, 1000);
+        connect(udpSocket, SIGNAL(readyRead()), SLOT(read()));
+        stateProc=true;
+        ui->pushButton->setText("Stop");//stop
+    }
+    else
+    {
+        disconnect(udpSocket, SIGNAL(readyRead()),this, SLOT(read()));
+        datafile.flush();
+        datafile.close();
+        stateProc=false;
+        ui->pushButton->setText("Start");
+    }
 }
 
 
 void MainWindow::read()
 {
     QByteArray Datagram;
-    QFile datafile("data.bin");
+    QDataStream in(&datafile);
 
-    do {
+    do
+    {
         Datagram.resize(udpSocket->pendingDatagramSize());
         udpSocket->readDatagram(Datagram.data(), Datagram.size());
-    } while(udpSocket->hasPendingDatagrams());
-
-    QDataStream in(&datafile);
-    in << Datagram;
+        in << Datagram;
+    }
+    while(udpSocket->hasPendingDatagrams());
 }
